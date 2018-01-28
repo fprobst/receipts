d9f4856bdda98d6e6e87f7db587cce73efa93022
fprobst <flo@probstl.de>
2018-01-28 22:15:56 +0100

    Login with google working

--- reciept-service/src/main/java/de/probstl/recieptservice/RecieptServiceApplication.java
+++ reciept-service/src/main/java/de/probstl/recieptservice/RecieptServiceApplication.java
@@ -3,24 +3,23 @@ package de.probstl.recieptservice;
 import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-public class RecieptServiceApplication {
+@EnableOAuth2Sso
+public class RecieptServiceApplication  {
+
+	@Bean
+	@ConfigurationProperties("spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecieptServiceApplication.class, args);
-	}
-	
-	@Bean
-	@ConfigurationProperties("app.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
 	}
 }
---
+++ reciept-service/src/main/java/de/probstl/recieptservice/controller/UserRestController.java
@@ -0,0 +1,17 @@
+package de.probstl.recieptservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/")
+public class UserRestController {
+
+	@RequestMapping(value = "user", method = RequestMethod.GET)
+	public Principal user(Principal user) {
+		return user;
+	}
+}
